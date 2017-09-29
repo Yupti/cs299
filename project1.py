02a4d96b18ef7500b56c2dbfdb318a1e00b9a94c
Aaron Reyes <aaron.nreyes@yahoo.com>
2017-09-29 11:01:25 -0700

    Update project1.py

--- project1.py
+++ project1.py
@@ -1,12 +1,16 @@
-string = "This feels of food"
-acronym = ""
-lists = string.split()
-rejected = ["and","by","in","of","on"]
-for i in lists:
-  canPrint = True
-  for j in rejected:
-    if i == j:
-      canPrint = False
-  if canPrint == True:
-    acronym += i[0].upper()
-print(acronym)
+def main():  
+  sentence = input("Enter a sentence to turn into an acronym")
+  acronymMaker(sentence)
+
+def acronymMaker(sentence):
+  acronym = ""
+  lists = sentence.split()
+  rejected = ["and","by","in","of","on"]
+  for i in lists:
+    canPrint = True
+    for j in rejected:
+      if i == j:
+        canPrint = False
+    if canPrint == True:
+      acronym += i[0].upper()
+  print(acronym)

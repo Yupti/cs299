947008c68c95770d0e9d77a37e45349c606ad3d4
Aaron Reyes <anreyes@cpp.edu>
2017-10-01 15:42:16 -0700

    part 2 finished

--- project1.py
+++ project1.py
@@ -1,6 +1,7 @@
 def main():  
-  sentence = input("Enter a sentence to turn into an acronym")
-  acronymMaker(sentence)
+  #sentence = input("Enter a sentence to turn into an acronym: ")
+  #acronymMaker(sentence)
+  #perfectNumberChecker(28)
 
 def acronymMaker(sentence):
   acronym = ""
@@ -14,3 +15,24 @@ def acronymMaker(sentence):
     if canPrint == True:
       acronym += i[0].upper()
   print(acronym)
+
+def perfectNumberChecker(number):
+  numList = [] # for holding the factors
+  start = 1
+  total = 0
+  print("Number for testing is:", number)
+  while start != number:
+    if number % start == 0:
+      numList.append(start)
+      total += start
+    start += 1
+  if total != number:
+    print("This number is not perfect.")
+  else:
+    print(number, "is a perfect number")
+    last = numList[-1]
+    for i in numList:
+      if i == last:
+        print(i, "=", number)
+      else:
+        print(i, "+ ", end = '')

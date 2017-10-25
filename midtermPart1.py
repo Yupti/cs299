4289f8819b398594d0365a20223165d5ba772b38
Aaron Reyes <anreyes@cpp.edu>
2017-10-24 21:47:10 -0700

    finished

--- midtermPart1.py
+++ midtermPart1.py
@@ -1,9 +1,8 @@
 def main():
-    print("test")
     checks("Jack", 10, 15, 20, 18, 7)
     checks("Joan", 10, 36)
     checks("David", 20, 3,5,2,6)
-    checks("Diana", 20, 0)
+    checks("Diana", 20)
 
 def checks(name, baseFee, *args):
     totalChecks = 0
@@ -13,24 +12,28 @@ def checks(name, baseFee, *args):
     for arg in args: # counts how many checks total
         totalChecks += arg
     totalChecks -= 20
-    if totalChecks > 0:
+    if totalChecks > 0: 
         totalFee += (20 * 0.10)
-    else:
+    else: 
         totalChecks += 20
         totalFee += (totalChecks * 0.10)
+        totalChecks -= totalChecks 
     totalChecks -= 30
     if totalChecks > 0:
         totalFee += (30 * 0.07)
     else:
         totalChecks += 30
         totalFee += (totalChecks * 0.07)
+        totalChecks -= totalChecks 
     totalChecks -= 10
     if totalChecks > 0:
-        totalFee += (10 * 0.05)
+        totalFee += (totalChecks * 0.05)
     else:
         totalChecks += 10
         totalFee += (totalChecks * 0.05)
-        
+        totalChecks -= totalChecks 
+
+    totalFee = round(totalFee, 2)
     print("Name:", name, "\tTotal fee:", totalFee)
     
         

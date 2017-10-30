2e3be4e31a0064491eba4a809ff01f32a2de279d
Aaron Reyes <anreyes@cpp.edu>
2017-10-30 09:59:58 -0700

    p3 change

--- project3part1RA.py
+++ project3part1RA.py
@@ -55,11 +55,11 @@ def drawing(cats):
     t.speed("fastest")
     t.hideturtle()
     t.penup()
-    t.goto(-400, -250)
+    t.goto(-400, -275)
     t.setheading(180)
     t.forward(30)
     t.write(0)
-    t.goto(-400, -250)
+    t.goto(-400, -275)
     t.pendown()
     t.setheading(90)
     
@@ -73,7 +73,7 @@ def drawing(cats):
         t.forward(30)
         t.setheading(90)
         
-    t.goto(-400, -250)
+    t.goto(-400, -275)
     t.setheading(0)
     for i in holder: # horizontal draw
         t.forward(80)
@@ -86,7 +86,7 @@ def drawing(cats):
         t.setheading(0)
         t.pendown()
     
-    t.goto(-400, -250)
+    t.goto(-400, -275)
     t.setheading(90)
     for i in cats: # for each bar
         val = (i / 3400) * 600
@@ -98,13 +98,13 @@ def drawing(cats):
         t.setheading(90)
 
     t.penup()
-    t.goto(-400, -250)
+    t.goto(-400, -275)
     t.forward(300)
     t.setheading(180)
     t.forward(150)
     t.write("# of earthquakes")
     
-    t.goto(-400, -250)
+    t.goto(-400, -275)
     t.setheading(0)
     t.forward(400)
     t.setheading(270)
@@ -112,7 +112,7 @@ def drawing(cats):
     t.write("Magnitude of earthquake")
 
     t.setheading(90)
-    t.forward(660)
+    t.forward(630)
     t.write("Histogram of Earthquakes (past 30 days)")
     
 if __name__ == "__main__":

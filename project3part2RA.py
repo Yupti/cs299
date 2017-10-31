6c3f6cd9848dabc9c2b15ee4e068e8c8d60187cb
Aaron Reyes <anreyes@cpp.edu>
2017-10-31 13:42:06 -0700

    finished part 3

--- project3part2RA.py
+++ project3part2RA.py
@@ -4,9 +4,7 @@ def main():
     names = "names.txt"
     codes = "codes.txt"
     compressed = readAndCreate(names, codes)
-    #addNew("Samwoo", 12, compressed)
-    #verifyName("2amwoo", compressed)
-    verifyPW("11111111112", compressed)
+    addNew("samwoo","1111111A1#a112", compressed)
 
 def readAndCreate(names, codes):
     L1 = []
@@ -27,8 +25,11 @@ def readAndCreate(names, codes):
     compressed = dict(zip(L1, L2))
     return compressed
 
-#def addNew(name, pw, dBase):
-    
+def addNew(name, pw, dBase):
+    if verifyName(name, dBase) and verifyPW(pw, dBase):
+        print("Requirements met, able to add to dictionary.")
+    else:
+        print("Either username or password does not meet a requirement, unable to add new entry.")
 
 def verifyName(name, dBase):
     if name[0].isalpha():
@@ -44,6 +45,7 @@ def verifyName(name, dBase):
     return True
 
 def verifyPW(pw, dBase):
+    checker = 0
     if len(pw) < 8:
         print("Password is too small, unable to use.")
         return False
@@ -53,8 +55,23 @@ def verifyPW(pw, dBase):
                 print("This password already exists.")
                 return False
         specials = ['#','$', '^', '*', '+', '=', '-', '_']
-    
-        
+        for sign in specials:
+            if sign in pw:
+                checker += 1
+                break
+        if any(x.isupper() for x in pw):
+            checker += 1
+        if any(x.islower() for x in pw):
+            checker += 1
+        if any(x.isdigit() for x in pw):
+            checker += 1
+            
+    if checker < 3:
+        print("The new password does not meet at least 3 requirements, unable to use.")
+        return False
+    else:
+        print("The new password can be used.")
+        return True
     
 if __name__ == "__main__":
     main()
